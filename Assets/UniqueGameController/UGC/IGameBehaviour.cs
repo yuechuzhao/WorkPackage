4c1457e8e0054818468348da9a137b2755e9668a
yuechuzhao <oisiiryouri@outlook.com>
2015-10-18 19:13:45 +0800

    Unique Game Controller

---
+++ Assets/UniqueGameController/UGC/IGameBehaviour.cs
@@ -0,0 +1,84 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace HardGames.UniqueController {
+	public interface IGameBehaviour{
+		void Awake();
+		void Start();
+		void Update();
+		void FixedUpdate();
+		void LateUpdate();
+		void OnEnable();
+		void OnDisable();
+		void OnDestroy();
+
+		void OnGUI();
+
+		void DestroySelf();
+	}
+
+	public class GameBehaviour {
+
+		private bool active_;
+
+		public GameObject Target {get; set;}
+
+		public GameBehaviour(){
+			Awake();
+			OnEnable();
+
+		}
+
+		~GameBehaviour(){
+		}
+
+		public void DestorySelf(){
+			if (Target != null){
+				GameObject.Destroy(Target);
+			} 
+			OnDestroy();
+		}
+
+
+		public virtual void Awake(){
+		}
+
+		public virtual void Start(){
+		}
+
+		public virtual void Update(){
+		}
+
+		public virtual void FixedUpdate(){
+		}
+
+		public virtual void LateUpdate(){
+		}
+
+		public virtual void OnGUI(){
+		}
+
+		public virtual void OnEnable(){
+		}
+
+		public virtual void OnDisable(){
+		}
+
+		public virtual void OnDestroy(){
+		}
+
+		public void SetActive(bool value){
+			if (active_ == value) return;
+			active_ = value;
+			if (active_) 
+				OnEnable();
+			else 
+				OnDisable();
+			if (Target != null)
+				Target.SetActive(value);
+		}
+
+		// TODO: lots of function not here, e.g. OnCollisionEnter. etc.
+	}
+}
+
---
+++ Assets/UniqueGameController/UGC/UniqueGameController.cs
@@ -0,0 +1,83 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace HardGames.UniqueController {
+
+	// only one monobehaviour in game 
+	public class UniqueGameController : MonoBehaviour {
+
+		static List<IGameBehaviour> allBehaviours;
+
+		// manually do this action
+		public static void AddGameBehaviour(IGameBehaviour g){
+			if (!allBehaviours.Contains(g)){
+				g.Start();
+				allBehaviours.Add(g);
+			}
+		}
+		// manually do this action
+		public static void RemoveGameBehaviour(IGameBehaviour g){
+			if (allBehaviours.Contains(g)){
+				g.DestroySelf();
+				allBehaviours.Remove(g);
+			}
+		}
+
+		void Awake(){
+			allBehaviours.Clear();
+			allBehaviours = new List<IGameBehaviour>();
+		}
+		
+		protected virtual void Start(){
+		}
+		
+		void OnEnable(){
+		}
+		
+		void OnDisable(){
+		}
+
+		void OnGUI(){
+			for (int i = 0; i < allBehaviours.Count; i++) {
+				allBehaviours[i].OnGUI();
+			}
+		}
+		
+		void Update(){
+			for (int i = 0; i < allBehaviours.Count; i++) {
+				allBehaviours[i].Update();
+			}
+		}
+		
+		void LateUpdate(){
+			for (int i = 0; i < allBehaviours.Count; i++) {
+				allBehaviours[i].LateUpdate();
+			}
+		}
+		
+		void FixedUpdate(){
+			for (int i = 0; i < allBehaviours.Count; i++) {
+				allBehaviours[i].FixedUpdate();
+			}
+		}
+		
+		void OnDestroy(){
+			for (int i = 0; i < allBehaviours.Count; i++) {
+				allBehaviours[i].DestroySelf();
+			}
+			allBehaviours.Clear();
+		}
+		
+		void OnLevelWasLoaded(int level){
+		}
+
+		void OnLevelWasLoaded(string levelName){
+		}
+		
+		void OnApplicationFocus(bool flag){
+		}
+	}
+
+}
+
